690f377259064a55273c27ceeee8443700e8fa63
Subhi2911 <msubhasmita29@gmail.com>
2025-06-22 19:01:40 +0530

    Adding a default image for no imaged news and including real time news with previous and next button on screen

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,16 +4,16 @@ export class NewsItem extends Component {
   render() {
     let {title,description,imageUrl,newsUrl}=this.props;
     return (
-      <div className='my-3'>
-        <div className="card" style={{width:" 18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}.</p>
-                <a href={newsUrl} target='blank' className="btn btn-sn btn-primary">Read More</a>
+        <div className='my-3'>
+            <div className="card" style={{width:" 18rem"}}>
+                <img src={imageUrl?imageUrl:"DefaultNewsImage.jpg"} className="card-img-top" alt="..."/>
+                <div className="card-body" >
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}.</p>
+                    <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sn btn-dark">Read More</a>
+                </div>
             </div>
         </div>
-      </div>
     )
   }
 }

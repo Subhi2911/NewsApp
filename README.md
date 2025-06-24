c24087da45b52fb4eab805ce40dffc20be2f62bb
Subhi2911 <msubhasmita29@gmail.com>
2025-06-24 12:39:04 +0530

    Adding the infinite scroll bar and editing the README.md file

--- README.md
+++ README.md
@@ -1,70 +1,23 @@
-# Getting Started with Create React App
+# learning react by building projects.
+This would be my second project wih react.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Learning class based components
+This project includes the usade of class-based-components of react.js
 
-## Available Scripts
+# Name of the project/website
+NewsDe-Lite
 
-In the project directory, you can run:
+# Logic behind the name of project
+News obviously stands for delivering news and current events.
 
-### `npm start`
+De-Lite sounds like “delight,” which means pleasure or joy.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+So, NewsDe-Lite means news that delights you — delivering news in a way that’s enjoyable, light, and easy to consume.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+It suggests your site brings fresh, interesting, and user-friendly news content that makes readers feel good rather than overwhelmed.
 
-### `npm test`
+# News + Delight = NewsDe-Lite = News that delights you!
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+# About Project
+As the name suggests, the project basically deals with conveying news headlines to its users.
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/components/News.js
+++ src/components/News.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component"
 
 // da1abdb15f6e4db8a745a9cc7047104a
 // 629c256e8600416e9087fb1b1f8a8206
 export default class News extends Component {
+    api= '629c256e8600416e9087fb1b1f8a8206';
     static defaultProps = {
         country:'in',
         pageSize:6,
@@ -20,8 +22,12 @@ export default class News extends Component {
         //search: PropTypes.string,
     }
 
-    constructor(){
-        super();
+    capitalizer(string){
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    constructor(props){
+        super(props);
         console.log("Hello I am constructor from news component");
         this.state={
             articles: [],
@@ -29,6 +35,7 @@ export default class News extends Component {
             page:1,
             totalResults: 0,
         }
+        document.title= `NewsDe-Lite || ${this.capitalizer(this.props.category)}`;
     }
     // buildUrl=()=>{
     //     if (this.props.useEverything){
@@ -40,7 +47,7 @@ export default class News extends Component {
     // }
 
     async updateNews(){
-        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=da1abdb15f6e4db8a745a9cc7047104a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.api}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({loading:true});
         let data= await fetch(url);
         let parsedData= await data.json()
@@ -49,19 +56,11 @@ export default class News extends Component {
             totalResults:parsedData.totalResults || 0,
             articles: parsedData.articles || [],
             loading:false ,
+            
         })
     }
     
     async componentDidMount(){
-        // let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=da1abdb15f6e4db8a745a9cc7047104a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-        // this.setState({loading:true});
-        // let data= await fetch(url);
-        // let parsedData= await data.json()
-        // console.log(parsedData);
-        // this.setState({articles: parsedData.articles || [], 
-        //     totalResults:parsedData.totalResults || 0,
-        //     loading:false ,
-        // })
         this.updateNews()
     }
 
@@ -78,36 +77,65 @@ export default class News extends Component {
         }
     }
 
-     handlePreviousClick=async()=>{
-        this.setState({page:this.state.page - 1});
-        this.updateNews();
-    }
+    fetchMoreData=async()=>{
 
-    handleNextClick=async()=>{
-        this.setState({page:this.state.page +1})
-        this.updateNews();
+        this.setState({page:this.state.page +1 })
+        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.api}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+        
+        let data= await fetch(url);
+        let parsedData= await data.json()
+        console.log(parsedData);
+        this.setState({
+            totalResults:parsedData.totalResults || 0,
+            articles: this.state.articles.concat(parsedData.articles) || [],
+            
+        })
     }
+    //  handlePreviousClick=async()=>{
+    //     this.setState({page:this.state.page - 1});
+    //     this.updateNews();
+    // }
+
+    // handleNextClick=async()=>{
+    //     this.setState({page:this.state.page +1})
+    //     this.updateNews();
+    // }
 
   render() {
     return (
-        <div className='container my-3' >
-            <h3 className='text-center' style={{marginBottom:'1rem'} }>NewsDe-Lite Top Headlines</h3>
+       <>
+            <h3 className='text-center' style={{marginBottom:'1rem'} }>NewsDe-Lite Top Headlines from {this.capitalizer(this.props.category)}</h3>
             {this.state.loading && <Spinner/>}
-            <div className="row" >
-                {!this.state.loading && Array.isArray(this.state.articles) && this.state.articles.map((element)=>{
-                    return(
-                        <div className="col-md-3 " key={element.url} >
-                            <NewsItem title={element.title?element.title.slice(0,45)+"...":" "} description={element.description?element.description.slice(0,88)+"...": " "} imageUrl={element.urlToImage} newsUrl=
-                            {element.url} author={element.author?element.author:"Unknown"} date={element.publishedAt} source={element.source.name}/>
+            <InfiniteScroll
+                dataLength={this.state.articles.length}
+                next={this.fetchMoreData}
+                hasMore={this.state.articles.length < this.state.totalResults}
+                loader={<Spinner/>}
+                >
+                    <div className="container">
+                        <div className="row" >
+                            {this.state.articles.map((element)=>{
+                                return(
+                                    <div className="col-md-3 " key={element.url} >
+                                        <NewsItem 
+                                        title={element.title?element.title.slice(0,45)+"...":" "} 
+                                        description={element.description?element.description.slice(0,88)+"...": " "} 
+                                        imageUrl={element.urlToImage} 
+                                        newsUrl={element.url} 
+                                        author={element.author?element.author:"Unknown"} 
+                                        date={element.publishedAt} 
+                                        source={element.source.name}/>
+                                    </div>
+                                )
+                            })} 
                         </div>
-                    )
-                })} 
-                <div className="container d-flex justify-content-between">
+                    </div>
+            </InfiniteScroll>
+            {/* <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <=1 ?true:false} type="button" className="btn btn-dark " onClick={this.handlePreviousClick}> &larr; Previous</button>
                     <button disabled={ this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-dark " onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
-            </div>
-        </div>
+            </div> */}
+        </>
     )
   }
 }
